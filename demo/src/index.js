a50ba214bcbecad87d4a65bbb3b039020f017401
Michael Odling-Smee <michael.odling-smee@airelogic.com>
2019-08-16 22:01:49 +0100

    Refactoring to pass in data

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import PortfolioExplorer from '../../src/PortfolioExplorer';
 
-import PortfolioExplorer from '../../src'
+const PORTFOLIO = [
+  {title: 'AAA', scale: '$49.99', stocked: true, name: 'Football'},
+  {title: 'BBB', scale: '$49.99', stocked: true, name: 'Football'},
+  {title: 'CCC', scale: '$49.99', stocked: true, name: 'Football'},
+  {title: 'DDD', scale: '$49.99', stocked: true, name: 'Football'},
+  {title: 'EEE', scale: '$49.99', stocked: true, name: 'Football'},
+  {title: 'FFF', scale: '$49.99', stocked: true, name: 'Football'},
+  {title: 'GGG', scale: '$49.99', stocked: true, name: 'Football'},
+];
 
 class Demo extends Component {
-  render() {
+  render() {    
     return <div>
       <h1>Portfolio Explorer Demo</h1>
-      <PortfolioExplorer/>
+      <PortfolioExplorer portfolio={PORTFOLIO} title="My great portfolio" />
     </div>
   }
 }
--- src/PortfolioExplorer.js
+++ src/PortfolioExplorer.js
@@ -5,10 +5,15 @@ import OversighArcSVG from './OversighArcSVG';
 import VerticalGuideSVG from './VerticalGuideSVG';
 import HorizontalGuideSVG from './HorizontalGuideSVG';
 import AreaArcSVG from './AreaArcSVG';
+import PropTypes from 'prop-types';
 
 class PortfolioExplorer extends Component {
 
     render() {
+        let title = this.props.title;
+        let portfolio = this.props.portfolio;
+        console.log("Here");
+        console.log(portfolio);
         const xmin = -250;
         const ymin = -250;
         const xmax = 250;
@@ -16,10 +21,9 @@ class PortfolioExplorer extends Component {
         const guides = true;
         var xlines = [-250,-200, -150, -100, -50, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500];
         var ylines = [-250,-200, -150, -100, -50, 0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500];
-        var projectNames = ["Aaaa", "Bbbb", "Cccc", "Dddd", "Eeee", "Ffff", "Gggg"]
         var xguides = guides ? xlines.map(function(offset) { return (<VerticalGuideSVG y1={ymin} y2={ymax} xoff={offset}/>); }) : null;
         var yguides = guides ? ylines.map(function(offset) { return (<HorizontalGuideSVG x1={xmin} x2={xmax} yoff={offset}/>); }) : null;
-        const projectNumber = projectNames.length;
+        const projectNumber = portfolio.length;
         const fullProjDeg = 360 / projectNumber;
         const projDeg = 300 / projectNumber;
         const r1 = 60
@@ -34,11 +38,12 @@ class PortfolioExplorer extends Component {
             var strokeWidth = (strokeWidthMax / projectNumber) * Math.round(Math.random()*projectNumber);
             var r2 = r1 + 0 + strokeWidth / 2;
             var pRot = p*fullProjDeg;
-            var name = projectNames[p] ? projectNames[p] : "project " + (p+1);
+            var name = portfolio[p] ? portfolio[p].title : "project " + (p+1);
             projects.push(<AreaArcSVG r1={r1} r2={r2} deg={projDeg} rot={pRot} name={name} strokeWidth={strokeWidth}/>);
         }
         return (
             <React.Fragment>
+                <h2>{title}</h2>
                 <div style={divStyle}>
                 <svg viewBox="-250 -250 500 500" preserveAspectRatio="xMinYMin meet">
                     {xguides}
@@ -56,5 +61,9 @@ class PortfolioExplorer extends Component {
         );
     }
 }
+
+// PortfolioExplorer.propTypes = {
+//     portfolio: PropTypes.isRequired,
+// };
 
 export default PortfolioExplorer;
