acce25369f6118dd44bea6723acc28338adb40aa
Michael Odling-Smee <michael.odling-smee@airelogic.com>
2019-11-02 20:44:54 +0000

    Coping with some missing data

--- src/TootipOverlay.js
+++ src/TootipOverlay.js
@@ -22,17 +22,25 @@ class ToolTipOverlay extends Component {
                 teamAvatars.push(<Avatar key={index} name={person.name} size={40} round="4px" email={person.email} className="teamAvatar"/>);
             });
         }
+        var hasTeamMembers = responsiblePerson || teamAvatars.length > 0;
         return (
             <React.Fragment>
                 {visible &&
 
                     <div id="tooltip" style={{left: `${this.props.x + xOffset}px`, top: `${this.props.y + yOffset}px`}}>
                         <h2>{this.props.tooltipInfo.title}</h2>
+                        {this.props.tooltipInfo.customer &&
+                            <div className="subtle">Customer: <span className="bold">{this.props.tooltipInfo.customer}</span></div>
+                        }
                         <p>{this.props.tooltipInfo.description}</p>
-                        {responsiblePerson &&
+                        {hasTeamMembers &&
                             <div className="team">
-                                <Avatar name={this.props.tooltipInfo.responsiblePerson.name} size={40} round="4px" email={this.props.tooltipInfo.responsiblePerson.email}  className="responsiblePersonAvatar" />
-                                <div className="personName">{this.props.tooltipInfo.responsiblePerson.name}</div>
+                                {responsiblePerson &&
+                                    <React.Fragment>
+                                        <Avatar name={this.props.tooltipInfo.responsiblePerson.name} size={40} round="4px" email={this.props.tooltipInfo.responsiblePerson.email}  className="responsiblePersonAvatar" />
+                                        <div className="personName">{this.props.tooltipInfo.responsiblePerson.name}</div>
+                                    </React.Fragment>
+                                }
                                 <div className="teamMembers">{teamAvatars}</div>
                             </div>               
                         }   
